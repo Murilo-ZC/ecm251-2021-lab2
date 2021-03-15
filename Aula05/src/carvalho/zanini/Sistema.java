a6cdcff0814a7a45dca5d9aca7b5d9a1afaec62d
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-15 10:14:14 -0300

    Criação do construtor da Sistema

--- Aula05/src/carvalho/zanini/Sistema.java
+++ Aula05/src/carvalho/zanini/Sistema.java
@@ -1,7 +1,31 @@
 package carvalho.zanini;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+
     public void executar(){
-        System.out.println("Aqui!");
+        int opcao;
+        while(executarSistema){
+            exibirMenu();
+            opcao = scanner.nextInt();
+        }
+    }
+
+    public Sistema() {
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in);  //Cria um scanner para o teclado (entrada padrão)
+    }
+
+    private void exibirMenu() {
+        System.out.println("Bem vindo ao MauaBank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar dinheiro");
+        System.out.println("3 - Sacar dinheiro");
+        System.out.println("4 - Transferir dinheiro");
+        System.out.println("5 - Pagar conta (título)");
+        System.out.println("0 - Encerrar Sistema");
     }
 }
