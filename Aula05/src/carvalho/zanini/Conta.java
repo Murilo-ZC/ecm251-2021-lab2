2a0ffc23fc286e322b7f72a6fae1167d3475f727
Murilo Zanini de Carvalho <carvalho.zanini@gmail.com>
2021-03-15 09:47:12 -0300

    Inicio do projeto da aula

---
+++ Aula05/src/carvalho/zanini/Conta.java
@@ -0,0 +1,43 @@
+package carvalho.zanini;
+
+public class Conta {
+    //Atributos
+    Cliente cliente;
+    int numero;
+    double saldo;
+
+    //Métodos
+    void visualizarSaldo() {
+        System.out.println("Valor do saldo: R$" + this.saldo);
+    }
+
+    boolean sacar(double valor) {
+        if( this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    void depositar(double valor) {
+//        this.saldo = this.saldo + valor;
+        this.saldo += valor;
+    }
+
+    boolean transferirDinheiro(Conta destino, double valor){
+        if(this.sacar(valor)){
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "cliente=" + cliente.toString() +
+                ", numero=" + numero +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
